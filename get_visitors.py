1ec71823a7f70c6b37b7490bf71d91c0a8156abf
Steve Bargery <63074131+steven-bargery@users.noreply.github.com>
2020-07-24 17:06:56 -0400

    Create get_visitors.py

---
+++ get_visitors.py
@@ -0,0 +1,21 @@
+import json
+import boto3
+
+def lambda_handler(event, context):
+    # Get the service resource.
+    dynamodb = boto3.resource('dynamodb')
+    
+    # Get the table
+    table = dynamodb.Table('sbargery_visitors')
+
+    # Get visitor count - takes 6 hours to update
+    #visitor_count = table.item_count
+
+    # Use scan to get visitor count - more reliable count
+    visitors = table.scan()
+    visitor_count = visitors['Count']
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(visitor_count)
+    }
