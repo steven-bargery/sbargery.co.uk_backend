48ca88c47afe8c9d6ddb5f854a5a962612d166e6
steven-bargery <sbargery@gmail.com>
2020-08-05 16:49:03 -0400

    updated the visitor count to just a single item in the DB

--- get_visitors.py
+++ get_visitors.py
@@ -1,21 +1,23 @@
-import json
 import boto3
 
 # Get the service resource.
 dynamodb = boto3.resource('dynamodb')
 
 # Get the table
-table = dynamodb.Table('sbargery_visitors')
+table = dynamodb.Table('sbargery_visitor_count')
 
 def lambda_handler(event, context):
     # Get visitor count - takes 6 hours to update
     #visitor_count = table.item_count
 
     # Use scan to get visitor count - more reliable count
-    visitors = table.scan()
-    visitor_count = visitors['Count']
+    #visitors = table.scan()
+    #visitor_count = visitors['Count']
+
+    # Update 5th Aug 2020 - now tracking visitor count with just a single item
+    visitor_count = table.get_item(Key={'visitor_key': "count"})
 
     return {
         'statusCode': 200,
-        'body': visitor_count
+        'body': int(visitor_count['Item']['value'])
     }
